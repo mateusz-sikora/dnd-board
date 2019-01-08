3d61b989c1fa60117690e7b3fad7f8722492766a
Mateusz Sikora <m.sikora@skytale.pl>
2019-01-08 15:44:54 +0100

    Updates

---
+++ precache-manifest.06aefb46de61edc9c589c2fd3dab8f15.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf318bed32d3a4a5fd5d",
+    "url": "/dnd-board/static/js/runtime~main.bf318bed.js"
+  },
+  {
+    "revision": "de23c21f9f510032c94a",
+    "url": "/dnd-board/static/js/main.de23c21f.chunk.js"
+  },
+  {
+    "revision": "40447da24ae52442562e",
+    "url": "/dnd-board/static/js/1.40447da2.chunk.js"
+  },
+  {
+    "revision": "033a6285827951d26949b3369be5e84d",
+    "url": "/dnd-board/index.html"
+  }
+];
